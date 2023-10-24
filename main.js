551ee7debf1c253fd05237c912cf39495154e910
lroux07 <lroux@edenschool.fr>
2023-10-24 22:27:34 +0200

    Finish seconde section main

---
+++ main.js
@@ -0,0 +1,8 @@
+let tachesInput = document.querySelector("#tachesInput");
+let inputBtn = document.querySelector("#inputBtn");
+let ol = document.querySelector("#ol");
+
+function liste() {
+  ol.innerHTML += `<li>${tachesInput.value}</li>`;
+}
+inputBtn.addEventListener("click", liste)
