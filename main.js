5f53f92e37c6b7b0fa43d4d29e41480cf75666cc
lroux07 <lroux@edenschool.fr>
2023-11-20 15:40:25 +0100

    Create array

--- main.js
+++ main.js
@@ -2,21 +2,38 @@ let tachesInput = document.querySelector("#tachesInput");
 let inputBtn = document.querySelector("#inputBtn");
 let ul = document.querySelector("#ul");
 
+const todos = [];
 const checkFunction = () => {
+  
+  if(tachesInput.value == "") {
+    return;
+  }
+  
+  todos.push(tachesInput.value)
+  console.log(todos)
+
   const inputList = document.createElement("INPUT");
   inputList.setAttribute("type", "checkbox");
   ul.appendChild(inputList);
 
   const labelCheked = document.createElement("label");
-  labelCheked.textContent=tachesInput.value;
+  labelCheked.textContent=todos;
   ul.appendChild(labelCheked);
 
   const br = document.createElement("br");
   ul.appendChild(br);
 
-  if(checkFunction.value.length === 0) {
-    return;
-  }
+  tachesInput.value = "";
+
+  // if(inputList.checked == true) {
+  //   labelCheked.style.textDecoration = "line-through";
+  // }
 }
+
+tachesInput.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    inputBtn.click();
+  }
+});
 
 inputBtn.addEventListener("click", checkFunction);
